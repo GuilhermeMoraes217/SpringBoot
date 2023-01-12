b362501064bc5509dd6ad7f005b17957f1583eec
Guilherme Moraes Furtado <guilhermefurtado78@gmail.com>
2023-01-12 18:48:34 -0300

    Tela de vendas quase finalizado

--- app/src/main/java/com/example/apirest/fragments/ProdutosFragment.java
+++ app/src/main/java/com/example/apirest/fragments/ProdutosFragment.java
@@ -1,18 +1,25 @@
 package com.example.apirest.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.apirest.R;
+import com.example.apirest.adapter.ViewPagerAdapter;
+import com.example.apirest.fragments.vendas.VendasDiaFragment;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class ProdutosFragment extends Fragment {
 
+    private TabLayout tab_Layout;
+    private ViewPager view_Pager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,7 +27,37 @@ public class ProdutosFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_produtos, container, false);
 
+        inicializarComponentes(view);
+        configTabsLayout();
+        configCliques(view);
 
         return view;
+    }
+
+
+    private void configCliques(View view){
+        //view.findViewById(R.id.ib_Voltar).setOnClickListener(view1 ->startActivity(new Intent(getContext(), HomeActivity.class)));
+    }
+
+
+    private void configTabsLayout(){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+        viewPagerAdapter.addFragment(new ProdutosFragment(), "Dia");
+        viewPagerAdapter.addFragment(new ProdutosFragment(), "Semana");
+        viewPagerAdapter.addFragment(new ProdutosFragment(), "Mes");
+        viewPagerAdapter.addFragment(new ProdutosFragment(), "Ano");
+
+        view_Pager.setAdapter(viewPagerAdapter);
+        view_Pager.setSaveEnabled(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tab_Layout.setElevation(0);
+        }
+        tab_Layout.setupWithViewPager(view_Pager);
+    }
+
+    private void inicializarComponentes(View view){
+        view_Pager =  view.findViewById(R.id.view_Pager);
+        tab_Layout =  view.findViewById(R.id.tab_Layout);
+
     }
 }
---
+++ app/src/main/java/com/example/apirest/fragments/produtos/ProdutosDiaFragment.java
@@ -0,0 +1,26 @@
+package com.example.apirest.fragments.produtos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.apirest.R;
+
+
+public class ProdutosDiaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_produtos_dia, container, false);
+
+
+
+        return view;
+    }
+}
