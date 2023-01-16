61a93ae9f6d9ac5a5cd3ccce9382bf101a3f58f0
Guilherme Moraes Furtado <guilhermefurtado78@gmail.com>
2023-01-16 10:16:37 -0300

    Tela de Checkout finalizada

--- app/src/main/java/com/example/apirest/fragments/CheckoutFragment.java
+++ app/src/main/java/com/example/apirest/fragments/CheckoutFragment.java
@@ -1,17 +1,26 @@
 package com.example.apirest.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.apirest.R;
+import com.example.apirest.adapter.ViewPagerAdapter;
+import com.example.apirest.fragments.checkout.CheckoutDiaFragment;
+import com.example.apirest.fragments.vendas.VendasDiaFragment;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class CheckoutFragment extends Fragment {
+
+    private TabLayout tab_Layout;
+    private ViewPager view_Pager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,8 +28,37 @@ public class CheckoutFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_checkout, container, false);
 
+        inicializarComponentes(view);
+        configTabsLayout();
+        configCliques(view);
 
 
         return view;
+    }
+
+    private void configCliques(View view){
+        //view.findViewById(R.id.ib_Voltar).setOnClickListener(view1 ->startActivity(new Intent(getContext(), HomeActivity.class)));
+    }
+
+
+    private void configTabsLayout(){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+        viewPagerAdapter.addFragment(new CheckoutDiaFragment(), "Dia");
+        viewPagerAdapter.addFragment(new CheckoutDiaFragment(), "Semana");
+        viewPagerAdapter.addFragment(new CheckoutDiaFragment(), "Mes");
+        viewPagerAdapter.addFragment(new CheckoutDiaFragment(), "Ano");
+
+        view_Pager.setAdapter(viewPagerAdapter);
+        view_Pager.setSaveEnabled(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tab_Layout.setElevation(0);
+        }
+        tab_Layout.setupWithViewPager(view_Pager);
+    }
+
+    private void inicializarComponentes(View view){
+        view_Pager =  view.findViewById(R.id.view_pager);
+        tab_Layout =  view.findViewById(R.id.tab_Layout);
+
     }
 }
