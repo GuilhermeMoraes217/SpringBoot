7d8ed7c281bfc0323648357d526277695cfffacb
Guilherme Moraes Furtado <guilhermefurtado78@gmail.com>
2023-02-03 10:01:04 -0300

    Atualizao String URL_001

--- app/src/main/java/com/example/apirest/utils/Apis.java
+++ app/src/main/java/com/example/apirest/utils/Apis.java
@@ -2,9 +2,7 @@ package com.example.apirest.utils;
 
 public class Apis {
 
-    //public static final String URL_001="http://10.0.0.100:8080/personas/"; // PC DO MEGAZORD
-    //public static final String URL_001="http://10.0.0.106:8080/personas/"; // PC DA SIGTEC INFORMATICA
-    public static final String URL_001="http://192.168.42.103:8080/personas/"; //http://localhost:8080/personas/listar
+    public static final String URL_001="http://192.168.1.109:8080/personas/"; // PC DA SIGTEC INFORMATICA
 
     public static PersonaService getPersonaService(){
         return  Cliente.getClient(URL_001).create(PersonaService.class);
