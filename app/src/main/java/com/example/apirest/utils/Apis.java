8db25278de8eb5df525acd581b6289f678c79264
Guilherme Moraes Furtado <guilhermefurtado78@gmail.com>
2023-02-08 10:35:15 -0300

    Atualicao do projeto para o notebook

--- app/src/main/java/com/example/apirest/utils/Apis.java
+++ app/src/main/java/com/example/apirest/utils/Apis.java
@@ -2,7 +2,7 @@ package com.example.apirest.utils;
 
 public class Apis {
 
-    public static final String URL_001="http://192.168.1.109:8080/personas/"; // PC DA SIGTEC INFORMATICA
+    public static final String URL_001="http://192.168.1.110:8080/personas/"; // Notebook
 
     public static PersonaService getPersonaService(){
         return  Cliente.getClient(URL_001).create(PersonaService.class);
