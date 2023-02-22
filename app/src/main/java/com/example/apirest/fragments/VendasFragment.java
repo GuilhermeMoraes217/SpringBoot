652764e00360fa5e2b34a4cd007e12112ed1b5b5
Guilherme Moraes Furtado <guilhermefurtado78@gmail.com>
2023-02-22 12:31:24 -0300

    Adicao de datas na fragmente vendas

--- app/src/main/java/com/example/apirest/fragments/VendasFragment.java
+++ app/src/main/java/com/example/apirest/fragments/VendasFragment.java
@@ -9,6 +9,7 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.apirest.R;
 import com.example.apirest.adapter.ViewPagerAdapter;
@@ -19,11 +20,18 @@ import com.example.apirest.fragments.vendas.VendasMesFragment;
 import com.example.apirest.fragments.vendas.VendasSemanaFragment;
 import com.google.android.material.tabs.TabLayout;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class VendasFragment extends Fragment {
 
     private TabLayout tab_Layout;
     private ViewPager view_Pager;
+
+    private String formattedDateAtual;
+
+    private TextView text_toolbar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,6 +40,7 @@ public class VendasFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_vendas, container, false);
 
         inicializarComponentes(view);
+        recuperandoData();
         configTabsLayout();
         configCliques(view);
 
@@ -41,6 +50,14 @@ public class VendasFragment extends Fragment {
 
     private void configCliques(View view) {
         //view.findViewById(R.id.ib_Voltar).setOnClickListener(view1 ->startActivity(new Intent(getContext(), HomeActivity.class)));
+    }
+
+    public void recuperandoData() {
+        Date d = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("dd/MMM/yyyy");
+        formattedDateAtual = df.format(d);
+
+        text_toolbar.setText(formattedDateAtual);
     }
 
 
@@ -62,6 +79,8 @@ public class VendasFragment extends Fragment {
     private void inicializarComponentes(View view) {
         view_Pager = view.findViewById(R.id.view_pager);
         tab_Layout = view.findViewById(R.id.tab_Layout);
+
+        text_toolbar = view.findViewById(R.id.txtToobarData);
 
     }
 }
