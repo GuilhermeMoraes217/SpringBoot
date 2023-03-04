b151fd5e21878ecbf416d128a2d937d1b0de3651
Guilherme Moraes Furtado <guilhermefurtado78@gmail.com>
2023-03-04 18:12:56 -0300

    finalizado a condicao de data  e recebimento do valor restante quando vencido

--- app/src/main/java/com/example/apirest/fragments/contas/dia/ContasDiaFragment.java
+++ app/src/main/java/com/example/apirest/fragments/contas/dia/ContasDiaFragment.java
@@ -1,7 +1,6 @@
 package com.example.apirest.fragments.contas.dia;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -18,11 +17,9 @@ import com.example.apirest.interfaces.CReceberService;
 import com.example.apirest.model.contas.CReceber;
 import com.example.apirest.utils.Apis;
 import com.example.apirest.utils.GetMask;
-import com.example.apirest.utils.HoraUtils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -43,11 +40,25 @@ public class ContasDiaFragment extends Fragment {
     /**
      * Atributos variados do layout
      */
-    private TextView textRelatorioContas, textTotalContasReceber, valorTotalAbertoText, valorTotalVencidoText, valorTotalLiquidadoText;
+    private TextView textRelatorioContas, textTotalContasReceber, valorTotalAbertoText, valorTotalVencidoText,
+            valorTotalLiquidadoText, valorTotalAbertoPagarText, valorTotalVencidoPagarText, valorTotalLiquidadoPagarText, valorTotalContasPagar;
+
+    //ATRIBUTOS DE VALORES A RECEBER
     private Double valorReceber = 0.0;
-    private Double valorAberto = 0.0;
-    private Double valorVencido = 0.0;
-    private Double valorLiquidado = 0.0;
+    private Double valorAbertoReceber = 0.0;
+    private Double valorVencidoReceber = 0.0;
+    private Double valorLiquidadoReceber = 0.0;
+
+    //ATRIBUTOS DOS VALORES A PAGAR
+    private Double valorPagar = 0.0;
+    private Double valorAbertoPagar = 0.0;
+    private Double valorVencidoPagar = 0.0;
+    private Double valorLiquidadoPagar = 0.0;
+
+    Date date1 = null;
+    Date date2 = null;
+    String formattedDateAtual;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,6 +74,42 @@ public class ContasDiaFragment extends Fragment {
         return view;
     }
 
+    public void convertendoStringInDate(String dataVecimento) {
+        // FUNCAO QUE RECUPERA A DATA ATUAL E DATA DE VENCIMENTO DO BANCO E CONVERTE DE STRING PARA DATE
+
+        //PEGANDO O VALOR DA DATA ATUAL
+        Date d = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        formattedDateAtual = "2023-02-14";
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            date1 = sdf.parse(formattedDateAtual);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            date2 = sdf.parse(dataVecimento);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void zerandoValoresContas() {
+        //ZERANDO OS VALORES
+        valorReceber = 0.0;
+        valorAbertoReceber = 0.0;
+        valorVencidoReceber = 0.0;
+        valorLiquidadoReceber = 0.0;
+
+        valorPagar = 0.0;
+        valorAbertoPagar = 0.0;
+        valorVencidoPagar = 0.0;
+        valorLiquidadoPagar = 0.0;
+    }
+
     public void listCReceber() {
         cReceberService = Apis.getCReceberService();
         Call<List<CReceber>> call = cReceberService.getrebecer();
@@ -70,58 +117,54 @@ public class ContasDiaFragment extends Fragment {
             @Override
             public void onResponse(Call<List<CReceber>> call, Response<List<CReceber>> response) {
                 if (response.isSuccessful()) {
-                    //ZERANDO OS VALORES
-                    valorReceber = 0.0;
-                    valorAberto = 0.0;
-                    valorVencido = 0.0;
-                    valorLiquidado = 0.0;
-
-                    //PEGANDO O VALOR DA DATA ATUAL
-                    Date d = new Date();
-                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-                    String formattedDateAtual = df.format(d);
+                    zerandoValoresContas(); // ZERANDO OS VALORES DAS CONTAS
 
                     List<CReceber> cRecebers1 = response.body();
+
                     for (CReceber cReceber : cRecebers1) {
+                        convertendoStringInDate(cReceber.getDtvencimento()); // RECEBENDOS AS DATAS EM FORMATO DATE
+
                         if (formattedDateAtual.equals(cReceber.getData())) {
                             if (!cReceber.getSituacao().equals("T")) {
                                 valorReceber += cReceber.getVl_restante();
-                                valorAberto += cReceber.getVl_restante();
+                                valorAbertoReceber += cReceber.getVl_restante();
                             }
 
                             if (cReceber.getSituacao().equals("T")) {
-                                valorLiquidado += cReceber.getVrecebido();
+                                valorLiquidadoReceber += cReceber.getVrecebido();
                             }
 
-                            String data1= formattedDateAtual;
-                            String data2=cReceber.getDtvencimento();
-                            String data3=cReceber.getData();
-                            Date dateAtual;
-                            Date dateVencimento;
-                            Date dateInicio;
-                            try {
-                                dateAtual=new SimpleDateFormat("yyyy-MM-dd").parse(data1);
-                                dateVencimento=new SimpleDateFormat("yyyy-MM-dd").parse(data2);
-                                dateInicio=new SimpleDateFormat("yyyy-MM-dd").parse(data3);
-                            } catch (ParseException e) {
-                                throw new RuntimeException(e);
-                            }
-
-                            if(dateAtual.after(dateInicio) && dateVencimento.before(dateAtual)) {
-                                /* historyDate <= todayDate <= futureDate */
-                                valorAberto += cReceber.getVl_restante();
+                            if (date1.compareTo(date2) > 0 && !cReceber.getSituacao().equals("T")) {
+                                System.out.println("Date1 is after Date2");
+                                valorVencidoReceber += cReceber.getVl_restante();
 
                             }
+                        } else {
 
+                            textTotalContasReceber.setText("R$ " + GetMask.getValor(0.0));
+                            valorTotalAbertoText.setText("R$ " + GetMask.getValor(0.0));
+                            valorTotalVencidoText.setText("R$ " + GetMask.getValor(0.0));
+                            valorTotalLiquidadoText.setText("R$ " + GetMask.getValor(0.0));
+
+
+                            valorTotalContasPagar.setText("R$ " + GetMask.getValor(0.0));
+                            valorTotalAbertoPagarText.setText("R$ " + GetMask.getValor(0.0));
+                            valorTotalVencidoPagarText.setText("R$ " + GetMask.getValor(0.0));
+                            valorTotalLiquidadoPagarText.setText("R$ " + GetMask.getValor(0.0));
                         }
                     }
                 }
 
                 textTotalContasReceber.setText("R$ " + GetMask.getValor(valorReceber));
+                valorTotalAbertoText.setText("R$ " + GetMask.getValor(valorAbertoReceber));
+                valorTotalVencidoText.setText("R$ " + GetMask.getValor(valorVencidoReceber));
+                valorTotalLiquidadoText.setText("R$ " + GetMask.getValor(valorLiquidadoReceber));
 
-                valorTotalAbertoText.setText("R$ " + GetMask.getValor(valorAberto));
-                valorTotalVencidoText.setText("R$ " + GetMask.getValor(valorVencido));
-                valorTotalLiquidadoText.setText("R$ " + GetMask.getValor(valorLiquidado));
+
+                valorTotalContasPagar.setText("R$ " + GetMask.getValor(valorPagar));
+                valorTotalAbertoPagarText.setText("R$ " + GetMask.getValor(valorAbertoPagar));
+                valorTotalVencidoPagarText.setText("R$ " + GetMask.getValor(valorVencidoPagar));
+                valorTotalLiquidadoPagarText.setText("R$ " + GetMask.getValor(valorLiquidadoPagar));
             }
 
             @Override
@@ -148,5 +191,10 @@ public class ContasDiaFragment extends Fragment {
         valorTotalAbertoText = view.findViewById(R.id.valorTotalAbertoText);
         valorTotalVencidoText = view.findViewById(R.id.valorTotalVencidoText);
         valorTotalLiquidadoText = view.findViewById(R.id.valorTotalLiquidadoText);
+
+        valorTotalContasPagar = view.findViewById(R.id.textView98);
+        valorTotalAbertoPagarText = view.findViewById(R.id.valorTotalAbertoPagarText);
+        valorTotalVencidoPagarText = view.findViewById(R.id.valorTotalVencidoPagarText);
+        valorTotalLiquidadoPagarText = view.findViewById(R.id.valorTotalLiquidadoPagarText);
     }
 }
